3267eaec8b400573bcd2494ea794b77f8c43a616
BRUNO JACCOUD <nf.brunojaccoudbrj@fab.mil.br>
2022-11-09 13:06:47 -0300

    feat: página exemplo MessageChat

--- src/domain/testes/pages/PageChatModel4.vue
+++ src/domain/testes/pages/PageChatModel4.vue
@@ -1,353 +1,102 @@
 <template>
-  <!-- https://vuetifyjs.com/en/components/timelines/#advanced -->
-  <div>
-    <v-container fluid>
-      <v-row justify="center">
-        <v-subheader>Today</v-subheader>
-
-        <v-expansion-panels popout>
-          <v-expansion-panel
-            v-for="(message, i) in messages"
-            :key="i"
-            hide-actions
-          >
-            <v-expansion-panel-header>
-              <v-row align="center" class="spacer" no-gutters>
-                <v-col cols="4" sm="2" md="1">
-                  <v-avatar size="36px">
-                    <img
-                      v-if="message.avatar"
-                      alt="Avatar"
-                      src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
-                    />
-                    <v-icon
-                      v-else
-                      :color="message.color"
-                      v-text="message.icon"
-                    ></v-icon>
-                  </v-avatar>
-                </v-col>
-
-                <v-col class="hidden-xs-only" sm="5" md="3">
-                  <strong v-html="message.name"></strong>
-                  <span v-if="message.total" class="grey--text">
-                    &nbsp;({{ message.total }})
-                  </span>
-                </v-col>
-
-                <v-col class="text-no-wrap" cols="5" sm="3">
-                  <v-chip
-                    v-if="message.new"
-                    :color="`${message.color} lighten-4`"
-                    class="ml-0 mr-2 black--text"
-                    label
-                    small
-                  >
-                    {{ message.new }} new
-                  </v-chip>
-                  <strong v-html="message.title"></strong>
-                </v-col>
-
-                <v-col
-                  v-if="message.excerpt"
-                  class="grey--text text-truncate hidden-sm-and-down"
-                >
-                  &mdash;
-                  {{ message.excerpt }}
-                </v-col>
-              </v-row>
-            </v-expansion-panel-header>
-
-            <v-expansion-panel-content>
-              <v-divider></v-divider>
-              <v-card-text v-text="lorem"></v-card-text>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </v-row>
-    </v-container>
-
-    <!--  -->
-
-    <v-card class="mx-auto" max-width="434" tile>
-      <v-img
-        height="100%"
-        src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
-      >
-        <v-row align="end" class="fill-height">
-          <v-col align-self="start" class="pa-0" cols="12">
-            <v-avatar class="profile" color="grey" size="164" tile>
-              <v-img
-                src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
-              ></v-img>
-            </v-avatar>
-          </v-col>
-          <v-col class="py-0">
-            <v-list-item color="rgba(0, 0, 0, .4)" dark>
-              <v-list-item-content>
-                <v-list-item-title class="text-h6">
-                  Marcus Obrien
-                </v-list-item-title>
-                <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-            <!--  -->
-            <v-list two-line>
-              <v-list-item>
-                <v-list-item-avatar>
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/john.png"
-                  ></v-img>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title>John Leider</v-list-item-title>
-                  <v-list-item-subtitle>Author</v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-action>
-                  <v-switch
-                    v-model="cycle"
-                    label="Cycle Slides"
-                    inset
-                  ></v-switch>
-                </v-list-item-action>
-              </v-list-item>
-            </v-list>
-          </v-col>
-        </v-row>
-      </v-img>
-    </v-card>
-    <!--  -->
-
-    <v-banner two-line>
-      <v-avatar slot="icon" color="deep-purple accent-4" size="40">
-        <v-icon icon="mdi-lock" color="white"> mdi-lock </v-icon>
-      </v-avatar>
-
-      Three line text string example with two actions. One to two lines is
-      preferable. Three lines should be considered the maximum string length on
-      desktop in order to keep messages short and actionable.
-
-      <template v-slot:actions>
-        <v-btn text color="deep-purple accent-4"> Action </v-btn>
-        <v-btn text color="deep-purple accent-4"> Action </v-btn>
-      </template>
-    </v-banner>
-    <!--  -->
-
-    <v-expansion-panels>
-      <v-expansion-panel>
-        <v-expansion-panel-header>
-          <template v-slot:default="{ open }">
-            <v-row no-gutters>
-              <v-col cols="4"> Trip name </v-col>
-              <v-col cols="8" class="text--secondary">
-                <v-fade-transition leave-absolute>
-                  <span v-if="open" key="0"> Enter a name for the trip </span>
-                  <span v-else key="1">
-                    {{ trip.name }}
-                  </span>
-                </v-fade-transition>
-              </v-col>
-            </v-row>
-          </template>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-text-field
-            v-model="trip.name"
-            placeholder="Caribbean Cruise"
-          ></v-text-field>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-
-      <v-expansion-panel>
-        <v-expansion-panel-header v-slot="{ open }">
-          <v-row no-gutters>
-            <v-col cols="4"> Location </v-col>
-            <v-col cols="8" class="text--secondary">
-              <v-fade-transition leave-absolute>
-                <span v-if="open" key="0"> Select trip destination </span>
-                <span v-else key="1">
-                  {{ trip.location }}
-                </span>
-              </v-fade-transition>
-            </v-col>
-          </v-row>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-row no-gutters>
-            <v-spacer></v-spacer>
-            <v-col cols="5">
-              <v-select
-                v-model="trip.location"
-                :items="locations"
-                chips
-                flat
-                solo
-              ></v-select>
-            </v-col>
-
-            <v-divider vertical class="mx-4"></v-divider>
-
-            <v-col cols="3">
-              Select your destination of choice
-              <br />
-              <a href="#">Learn more</a>
-            </v-col>
-          </v-row>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn text color="secondary"> Cancel </v-btn>
-            <v-btn text color="primary"> Save </v-btn>
-          </v-card-actions>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-
-      <v-expansion-panel>
-        <v-expansion-panel-header v-slot="{ open }">
-          <v-row no-gutters>
-            <v-col cols="4"> Start and end dates </v-col>
-            <v-col cols="8" class="text--secondary">
-              <v-fade-transition leave-absolute>
-                <span v-if="open">When do you want to travel?</span>
-                <v-row v-else no-gutters style="width: 100%">
-                  <v-col cols="6">
-                    Start date: {{ trip.start || 'Not set' }}
-                  </v-col>
-                  <v-col cols="6">
-                    End date: {{ trip.end || 'Not set' }}
-                  </v-col>
-                </v-row>
-              </v-fade-transition>
-            </v-col>
-          </v-row>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-row justify="space-around" no-gutters>
-            <v-col cols="3">
-              <v-menu
-                ref="startMenu"
-                :close-on-content-click="false"
-                :return-value.sync="trip.start"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="trip.start"
-                    label="Start date"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker v-model="date" no-title scrollable>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    text
-                    color="primary"
-                    @click="$refs.startMenu.isActive = false"
-                  >
-                    Cancel
-                  </v-btn>
-                  <v-btn
-                    text
-                    color="primary"
-                    @click="$refs.startMenu.save(date)"
-                  >
-                    OK
-                  </v-btn>
-                </v-date-picker>
-              </v-menu>
-            </v-col>
-
-            <v-col cols="3">
-              <v-menu
-                ref="endMenu"
-                :close-on-content-click="false"
-                :return-value.sync="trip.end"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="trip.end"
-                    label="End date"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker v-model="date" no-title scrollable>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    text
-                    color="primary"
-                    @click="$refs.endMenu.isActive = false"
-                  >
-                    Cancel
-                  </v-btn>
-                  <v-btn text color="primary" @click="$refs.endMenu.save(date)">
-                    OK
-                  </v-btn>
-                </v-date-picker>
-              </v-menu>
-            </v-col>
-          </v-row>
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
+  <!--  -->
+  <!-- <MessageChat /> -->
+  <div class="d-flex flex-grow-1 flex-column">
+    <MessageChat
+      :avatarSize="64"
+      :padraoData="'DD/MM/YYYY HH:mm:ss'"
+      :messages="messages"
+      :userInformations="userInformations"
+      :clearable="true"
+      :maxlength="150"
+      @forum-message="messageReceived($event)"
+      :hintText="`${$t('chat.message')}`"
+      :counterLength="true"
+      :placeholder="`${$t('chat.message')}`"
+    />
   </div>
+  <!--  -->
 </template>
 
 <script>
+import MessageChat from './MessageChat.vue';
+
 export default {
-  data: () => ({
-    messages: [
-      {
-        avatar: 'https://avatars0.githubusercontent.com/u/9064066?v=4&s=460',
-        name: 'John Leider',
-        title: 'Welcome to Vuetify!',
-        excerpt: 'Thank you for joining our community...',
+  components: {
+    MessageChat,
+  },
+  data() {
+    return {
+      messages: [
+        {
+          id: 1,
+          img: '/images/avatars/avatar1.svg',
+          name: 'Mattew',
+          section: 'Seção de Trabalho',
+          data: '2022-11-01T09:25:32Z',
+          msg: 'Percebemos, cada vez mais, que a crescente influência da mídia faz parte de um processo de gerenciamento das direções preferenciais no sentido do progresso.',
+          response: true,
+          readonly: false,
+          interactions: [
+            {
+              id: 1,
+              name: 'Fatima',
+              section: 'Social',
+              data: '2022-11-02T03:14:12Z',
+              msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum id diam ac fringilla. Pellentesque eu justo non mauris molestie faucibus. Sed ornare, nulla vel accumsan volutpat, lectus ante efficitur lacus, et maximus ipsum enim in dolor. Ut arcu ipsum, scelerisque at dapibus vel, eleifend quis metus. Donec condimentum convallis massa in luctus. Donec est nibh, bibendum sed quam in, posuere dignissim risus. Fusce ut tellus non arcu porta ultrices eu ac risus. Duis eget orci vel integer.',
+            },
+            {
+              id: 2,
+              name: 'Mattew',
+              section: 'Work',
+              data: '2022-11-03T01:00:01Z',
+              msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu euismod erat, et posuere mi. Sed pellentesque ex id enim euismod, consequat aliquet arcu porta. Suspendisse mattis, leo sit amet egestas blandit, ligula erat tempus justo, ut molestie.',
+            },
+            {
+              id: 3,
+              name: 'Xin',
+              section: 'Fun',
+              data: '2022-11-04T03:14:12Z',
+              msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In libero risus, pellentesque eu malesuada ut, accumsan eget felis. Praesent faucibus nec at.',
+            },
+          ],
+        },
+        {
+          id: 2,
+          img: '/images/avatars/avatar4.svg',
+          name: 'Fatima',
+          section: 'Seção Social',
+          data: '2022-11-02T19:17:12Z',
+          msg: 'Não obstante, a competitividade nas transações comerciais causa impacto indireto na reavaliação dos índices pretendidos.',
+          response: true,
+          readonly: true,
+          interactions: [],
+        },
+        {
+          id: 3,
+          img: '/images/avatars/avatar6.svg',
+          name: 'Xin',
+          section: 'Seção Social',
+          data: '2022-11-06T22:19:59Z',
+          msg: 'Caros amigos, a competitividade nas transações comerciais exige a precisão e a definição do levantamento das variáveis envolvidas.',
+          response: false,
+          readonly: false,
+          interactions: [],
+        },
+      ],
+      userInformations: {
+        name: 'Bruno Jaccoud',
+        section: 'Teste Teste',
       },
-      {
-        color: 'red',
-        icon: 'mdi-account-multiple',
-        name: 'Social',
-        new: 1,
-        total: 3,
-        title: 'Twitter',
-      },
-      {
-        color: 'teal',
-        icon: 'mdi-tag',
-        name: 'Promos',
-        new: 2,
-        total: 4,
-        title: 'Shop your way',
-        exceprt: 'New deals available, Join Today',
-      },
-    ],
-    lorem:
-      'Lorem ipsum dolor sit amet, at aliquam vivendum vel, everti delicatissimi cu eos. Dico iuvaret debitis mel an, et cum zril menandri. Eum in consul legimus accusam. Ea dico abhorreant duo, quo illum minimum incorrupte no, nostro voluptaria sea eu. Suas eligendi ius at, at nemore equidem est. Sed in error hendrerit, in consul constituam cum.',
-    cycle: true,
-    date: null,
-    trip: {
-      name: '',
-      location: null,
-      start: null,
-      end: null,
+    };
+  },
+  methods: {
+    messageReceived(event) {
+      console.log(event);
     },
-    locations: [
-      'Australia',
-      'Barbados',
-      'Chile',
-      'Denmark',
-      'Ecuador',
-      'France',
-    ],
-  }),
+    userReceived(event) {
+      console.log(event);
+    },
+  },
 };
 </script>
