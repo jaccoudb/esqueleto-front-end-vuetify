d82b9a684d203b965057f806676fc38ecfe96aba
Bruno Jaccoud <bruno.jaccoud@gmail.com>
2022-11-13 19:54:47 -0300

    feat: página de teste para componente upload.

---
+++ src/domain/testes/pages/PageUpload.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <UploadFiles />
+  </div>
+</template>
+
+<script>
+import UploadFiles from './UploadFiles.vue';
+export default {
+  components: {
+    UploadFiles,
+  },
+};
+</script>
