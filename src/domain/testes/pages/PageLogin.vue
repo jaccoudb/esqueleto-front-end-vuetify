688e21f77cc854cc023a6704029229461cb647fc
Bruno Jaccoud <bruno.jaccoud@gmail.com>
2022-11-20 19:49:08 -0300

    feat: página login

---
+++ src/domain/testes/pages/PageLogin.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!--  -->
+    <confirm-with-password
+      :persistent="true"
+      v-model="showConfirmPassword"
+      @confirm="confirm()"
+    />
+
+    <!-- Botões de Ação -->
+    <v-row align="center" justify="space-between" class="mx-auto">
+      <v-btn dark tile color="red darken-2" @click="clear">
+        {{ $t('cancel') }}
+      </v-btn>
+      <v-btn dark tile color="#009e0f" @click="submit">
+        {{ $t('confirm.tag') }}
+      </v-btn>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import ConfirmWithPassword from './ConfirmWithPassword.vue';
+export default {
+  components: {
+    ConfirmWithPassword,
+  },
+  data() {
+    return {
+      showConfirmPassword: false,
+    };
+  },
+  methods: {
+    clear() {
+      console.log('clear');
+    },
+    submit() {
+      console.log('teste');
+      this.showConfirmPassword = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  height: 200px;
+  width: 50%;
+  background-color: powderblue;
+}
+</style>
